9584a4662971257122af67744a4f5f5eaa7ed91b
Prasenjit Sutradhar <prasenjitsutradhar3433@gmail.com>
2022-10-10 00:32:10 +0530

    updating our directory page with Directory component and CategoryItem component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './categories.styles.scss'
+import Directory from './components/directory/directory.component';
 
 const App = () => {
   const categories = [
@@ -30,21 +30,7 @@ const App = () => {
   ];
 
   return (
-    <div className="categories-container">
-      {categories.map(({ id, title, imageUrl }) => {
-        return (
-          <div className="category-container" key={id}>
-            <div className="background-image" style={{
-              backgroundImage: `url(${imageUrl})`
-            }}></div>
-            <div className="category-body-container">
-              <h2>{title}</h2>
-              <p>Show Now</p>
-            </div>
-          </div>
-        );
-      })}
-    </div>
+    <Directory categories={categories}/>
   );
 };
 
