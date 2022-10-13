8b44ec469cbb20ef0ee48df9264b1546da551dd7
Prasenjit Sutradhar <prasenjitsutradhar3433@gmail.com>
2022-10-13 21:28:58 +0530

    storing user information in firestore

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
 import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+import { getFirestore, doc, getDoc, setDoc, connectFirestoreEmulator } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -23,3 +24,32 @@ const firebaseConfig = {
 
   export const auth = getAuth();
   export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+  export const db = getFirestore();
+
+  export const createUserDocumentFromAuth = async (userAuth) => {
+    // Check if there's an existing document reference inside this database instance
+    const userDocRef = doc(db, 'users', userAuth.uid);
+
+    // Now pass that document reference into getDoc to check if there's any instance of that reference:
+    const userSnapshot = await getDoc(userDocRef);
+ 
+    // if user data does not exist
+    if (!userSnapshot.exists()) {
+      const { displayName, email } = userAuth;
+      const createdAt = new Date();
+
+      // create or set the document with the data from userAuth in my collection
+      try {
+        await setDoc(userDocRef, {
+          displayName,
+          email,
+          createdAt
+        });
+      } catch (error) {
+        console.log('error creating the user', error.message);
+      }
+    }
+    
+    return userDocRef;
+  }
