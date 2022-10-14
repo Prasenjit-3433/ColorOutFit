08d495bd280a8d5e1f2d899572d3d922c41c5b7b
Prasenjit Sutradhar <prasenjitsutradhar3433@gmail.com>
2022-10-14 14:26:24 +0530

    building sign-up form in progress

---
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+
+const defaultFormFields = {
+    displayName: '',
+    email: '',
+    password: '',
+    confirmPassword: ''
+};
+
+const SignUpForm = () => {
+    const [formFields, setFormFields] = useState(defaultFormFields);
+    const { displayName, email, password, confirmPassword } = formFields;
+
+    console.log(formFields);
+
+    const handleChange = (event) => {
+        // destructure the name, value of that field from event.target
+        const { name, value } = event.target;
+        
+        // Since we're only going to be updating one input at a time, 
+        setFormFields({...formFields, [name]: value});
+    }
+
+    return (
+        <div>
+            <h1>Sign up with your email and password</h1>
+            <form onSubmit={() => {}}>
+                <label>Display Name</label>
+                <input type='text' required onChange={handleChange} name='displayName' value={displayName} />
+
+                <label>Email</label>
+                <input type='email' required onChange={handleChange} name='email' value={email} />
+
+                <label>Password</label>
+                <input type='password' required onChange={handleChange} name='password' value={password} />
+
+                <label>Confirm Password</label>
+                <input type='password' required onChange={handleChange} name='confirmPassword' value={confirmPassword} />
+
+                <button type="submit">Sign Up</button>
+            </form>
+        </div>
+    );
+}
+
+export default SignUpForm;
