af33be951a88ed9fcdb6c811d2c58c83c5ef0dd1
Prasenjit Sutradhar <prasenjitsutradhar3433@gmail.com>
2022-10-28 22:44:35 +0530

    navigation link to browse different category

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from 'react';
+import { useContext, useEffect, useState, Fragment } from 'react';
 import { useParams } from 'react-router-dom';
 
 import { CategoriesContext } from '../../contexts/categories.context';
@@ -16,14 +16,17 @@ const Category = () => {
     }, [category, categoriesMap]);
     
     return (
-        <div className='category-container'>
-            {
-                products && 
-                products.map((product)=> {
-                    return <ProductCard key={product.id} product={product}/>
-                })
-            }
-        </div>
+        <Fragment>
+            <h2 className="category-title">{category.toUpperCase()}</h2>
+            <div className='category-container'>
+                {
+                    products && 
+                    products.map((product)=> {
+                        return <ProductCard key={product.id} product={product}/>
+                    })
+                }
+            </div>
+        </Fragment>
     );
 }
 
