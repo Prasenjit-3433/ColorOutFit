92c197fb9c3efa4fad10cce9bfe40770379442e9
Prasenjit Sutradhar <prasenjitsutradhar3433@gmail.com>
2022-11-05 20:22:39 +0530

    useState is replaced by useReducer hook

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,15 +1,43 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useEffect, useReducer } from "react";
 
 import { onAuthStateChangedListener, createUserDocumentFromAuth } from '../utils/firebase/firebase.utils';
 
-// as the actual value you want to access
+// as the actual value you want to expose
 export const UserContext = createContext({
     currentUser: null,
     setCurrentuser: () => null,
 });
 
+export const USER_ACTION_TYPES = {
+    SET_CURRENT_USER: 'SET_CURRENT_USER'
+}
+
+const userReducer = (state, action) => {
+    const { type, payload } = action;
+    
+    switch (type) {
+        case USER_ACTION_TYPES.SET_CURRENT_USER: 
+            return {
+                ...state,
+                currentUser: payload
+            };
+
+        default: 
+            throw new Error(`Unhandled type ${type} in userReducer`);
+    }
+}
+
+const INITIAL_STATE = {
+    currentUser: null
+}
+
 export const UserProvider = ({ children }) => {
-    const [currentUser, setCurrentuser] = useState(null);
+    const [ { currentUser }, dispatch ] = useReducer(userReducer, INITIAL_STATE);
+    
+    const setCurrentuser = (user) => {
+        dispatch({ type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user });
+    }
+
     const value = { currentUser, setCurrentuser };
 
     // we want to run once the component mounts for the first time
