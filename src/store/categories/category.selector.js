92241b7b461db90add1e3ea88e8644f8050921c7
Prasenjit Sutradhar <prasenjitsutradhar3433@gmail.com>
2022-11-14 14:04:37 +0530

    used reselect library to cache categoriesMap used inside Category component

--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -1,6 +1,23 @@
-export const selectCategoriesMap = (state) => 
-state.categories.categories.reduce((acc, category) => {
+import { createSelector } from "reselect";
+
+// categoriesReducer's state
+const selectCategoriesReducerState = (state) => {
+  return state.categories};
+
+// gives categories array
+export const selectCategories = createSelector(
+  [selectCategoriesReducerState],
+  (categoriesReducerState) => {
+    return categoriesReducerState.categories}
+);
+
+// gives categoriesMap
+export const selectCategoriesMap = createSelector(
+  [selectCategories],
+  (categories) => {
+    return categories.reduce((acc, category) => {
     const { title, items } = category;
     acc[title.toLowerCase()] = items;
     return acc;
-  }, {});
+  }, {})}
+);
