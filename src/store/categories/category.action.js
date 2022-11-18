d03f419c8741bd5f970c9a34681249c2e7e9c494
Prasenjit Sutradhar <prasenjitsutradhar3433@gmail.com>
2022-11-18 18:26:05 +0530

    used redux-thunk to move the async based categories fething code into a thunk

--- src/store/categories/category.action.js
+++ src/store/categories/category.action.js
@@ -1,4 +1,29 @@
-import { CATEGORIES_ACTION_TYPES } from './category.types';
+import { CATEGORIES_ACTION_TYPES } from "./category.types";
 import { createAction } from "../../utils/reducer/reducer.utils";
 
-export const setCategories = (categoriesArray) => createAction(CATEGORIES_ACTION_TYPES.SET_CATEGORIES, categoriesArray);
+import { getCollectionAndDocuments } from '../../utils/firebase/firebase.utils';
+
+// Regular actions
+export const fetchCategoriesStart = () =>
+  createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START);
+
+export const fetchCategoriesSuccess = (categoriesArray) =>
+  createAction(
+    CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS,
+    categoriesArray
+  );
+
+export const fetchCategoriesFailed = (error) =>
+  createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED, error);
+
+// Thunk-action
+export const fetchCategoriesAsync = () => async (dispatch) => {
+    dispatch(fetchCategoriesStart());
+
+    try {
+        const categoriesArray = await getCollectionAndDocuments();
+        dispatch(fetchCategoriesSuccess(categoriesArray));
+    } catch (error) {
+        dispatch(fetchCategoriesFailed(error));
+    }
+}
