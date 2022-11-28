3f29d0867be774beb952236f8d85b39587dbf954
Prasenjit Sutradhar <prasenjitsutradhar3433@gmail.com>
2022-11-28 23:31:57 +0530

    made a serverless function that handles stripe-payment & hosted on Netlify-Function

--- src/components/payment-form/payment-form.component.jsx
+++ src/components/payment-form/payment-form.component.jsx
@@ -14,11 +14,36 @@ const PaymentForm = () => {
         if (!stripe || !elements) {
             return;
         }
+
+        const response = await fetch('/.netlify/functions/create-payment-intent', {
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ amount: 10000 })
+        }).then(res => res.json());
+
+        const { client_secret } = response.paymentIntent;
+        
+        const paymentResult = await stripe.confirmCardPayment(client_secret, {
+            payment_method: {
+                card: elements.getElement(CardElement),
+                billing_details: {
+                    name: 'Prasenjit Sutradhar'
+                }
+            }
+        });
+
+        if (paymentResult.error) {
+            alert(paymentResult.error.message);
+        } else if (paymentResult.paymentIntent.status === 'succeeded') {
+            alert('Payment Successful');
+        }
     }
 
     return (
         <PaymentFormContainer>
-            <FormContainer>
+            <FormContainer onSubmit={paymentHandler}>
                 <CardElement />
                 <Button buttonType={BUTTON_TYPE_CLASSES.inverted}>Pay Now</Button>
             </FormContainer>
