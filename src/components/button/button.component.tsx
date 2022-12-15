58f5fd54c96230c9b7428787a768d34d0661c6ba
Prasenjit Sutradhar <prasenjitsutradhar3433@gmail.com>
2022-12-15 13:15:36 +0530

    migration of components to typescript: button

--- src/components/button/button.component.tsx
+++ src/components/button/button.component.tsx
@@ -1,20 +1,26 @@
+import { FC, ButtonHTMLAttributes } from "react";
 import { BaseButton, GoogleSignInButton, InvertedButton, ButtonSpinner } from "./button.styles";
 
 //  Three types of button: default, inverted & google sign-in
-export const BUTTON_TYPE_CLASSES = {
-    base: 'base',
-    google: 'google-sign-in',
-    inverted: 'inverted',
+export enum BUTTON_TYPE_CLASSES {
+    base = 'base',
+    google = 'google-sign-in',
+    inverted = 'inverted',
 }
 
-const getButton = (buttonType = BUTTON_TYPE_CLASSES.base) => 
+const getButton = (buttonType = BUTTON_TYPE_CLASSES.base): typeof BaseButton => 
     ({
         [BUTTON_TYPE_CLASSES.base]: BaseButton,
         [BUTTON_TYPE_CLASSES.google]: GoogleSignInButton,
         [BUTTON_TYPE_CLASSES.inverted]: InvertedButton
     }[buttonType]);
 
-export const Button = ({ children, buttonType, isLoading, ...otherProps }) => {
+export type ButtonProps = {
+    buttonType?: BUTTON_TYPE_CLASSES;
+    isLoading?: boolean
+} & ButtonHTMLAttributes<HTMLButtonElement>
+
+export const Button: FC<ButtonProps> = ({ children, buttonType, isLoading, ...otherProps }) => {
     const CustomButton = getButton(buttonType);
 
     return (
