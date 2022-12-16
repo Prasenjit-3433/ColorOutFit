7bab741431344df762a11419dfd080f6b274b02e
Prasenjit Sutradhar <prasenjitsutradhar3433@gmail.com>
2022-12-16 13:02:14 +0530

    migration of components to typescript: directory

--- src/components/directory/directory.component.tsx
+++ src/components/directory/directory.component.tsx
@@ -1,5 +1,5 @@
 import DirectoryItem from '../directory-item/directory-item.component';
-import './directory.styles.scss'
+import { DirectoryContainer } from './directory.styles';
 
 const categories = [
   {
@@ -36,11 +36,11 @@ const categories = [
 
 const Directory = () => {
   return (
-    <div className="directory-container">
+    <DirectoryContainer>
       {categories.map((category) => {
         return <DirectoryItem category={category} key={category.id} />;
       })}
-    </div>
+    </DirectoryContainer>
   );
 };
 
