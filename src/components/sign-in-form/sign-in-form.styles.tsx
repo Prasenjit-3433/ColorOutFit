72f961c4f290a1d9f3597d94bd1ea6f5012a8dbf
Prasenjit Sutradhar <prasenjitsutradhar3433@gmail.com>
2022-12-23 12:19:35 +0530

    adjusted styles of sign-in & sign-up forms for mobile view

--- src/components/sign-in-form/sign-in-form.styles.tsx
+++ src/components/sign-in-form/sign-in-form.styles.tsx
@@ -8,9 +8,20 @@ export const SignUpContainer = styled.div`
     h2 {
         margin: 10px 0;
     }
+
+    @media screen and (max-width: 800px) {
+        padding: 20px;
+    }
 `
 
 export const ButtonsContainer = styled.div`
     display: flex;
     justify-content: space-between;
+
+    button {
+        @media screen and (max-width: 800px) {
+            padding: 5px;
+            color: white;
+        }
+    }
 `
